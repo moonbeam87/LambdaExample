f2dd703ae47fe5671bd7bc37dd2006e76b6da726
Dev <2021dpatel@gmail.com>
2020-12-21 11:04:47 -0500

    message processor added

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import boto3
         
 def lambda_handler(event, context):
     json_region = os.environ['AWS_REGION']
+    messages = event["Records"]
+    for message in messages:
+        messageID = message["messageId"]
+        body = message["body"]
+        #POST INTO NGINX
     print(event)
     return {
         "statusCode": 200,
